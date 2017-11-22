5af10153859e8a286c8c5a470ab1b813f5ea4c63
yichenroy <yi-chen.tsai@liferay.com>
2017-11-22 09:28:08 -0800

    LRQA-36161 Store namespace when storing testcase class names

--- poshi-runner/src/main/java/com/liferay/poshi/runner/PoshiRunnerContext.java
+++ poshi-runner/src/main/java/com/liferay/poshi/runner/PoshiRunnerContext.java
@@ -1149,7 +1149,7 @@ public class PoshiRunnerContext {
 			filePath);
 
 		if (classType.equals("test-case")) {
-			_testCaseClassNames.add(className);
+			_testCaseClassNames.add(namespace + "." + className);
 
 			if (rootElement.element("set-up") != null) {
 				Element setUpElement = rootElement.element("set-up");
