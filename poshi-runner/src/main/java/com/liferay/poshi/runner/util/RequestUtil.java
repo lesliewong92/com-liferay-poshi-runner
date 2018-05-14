4e150ac128268d95bc23fc6a1da8d5088fdb6774
Leslie Wong <leslie.wong@liferay.com>
2018-05-14 16:36:13 -0700

    LRQA-40561 Format request body to application/x-www-form-urlencoded if no 'Content-Type' is given

--- poshi-runner/src/main/java/com/liferay/poshi/runner/util/RequestUtil.java
+++ poshi-runner/src/main/java/com/liferay/poshi/runner/util/RequestUtil.java
@@ -22,6 +22,7 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
+import java.net.URLEncoder;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -139,6 +140,9 @@ public class RequestUtil {
 									"Invalid JSON: '" + requestBody + "'");
 							}
 						}
+					}
+					else {
+						requestBody = URLEncoder.encode(requestBody, "UTF-8");
 					}
 
 					httpURLConnection.setDoOutput(true);
